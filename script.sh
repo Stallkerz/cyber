37efc72e1079985ee75c7debf5e68fb3bea4228e
Stallkerz <howanleeplay2@mail.ru>
2023-10-18 16:59:54 +0500

    script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt install openssh-server ufw -y
 cp -r 40_custom /etc/grub.d/40_custom
+sed -i '1 s/CLASS="--class gnu-linux --class gnu --class os"/CLASS="--class gnu-linux --class gnu --class os --unrestricted"/' /etc/grub.d/10_linux
 grub-mkconfig -o /boot/grub/grub.cfg
 cp -r sshd_config /etc/ssh/sshd_config
 systemctl restart sshd
